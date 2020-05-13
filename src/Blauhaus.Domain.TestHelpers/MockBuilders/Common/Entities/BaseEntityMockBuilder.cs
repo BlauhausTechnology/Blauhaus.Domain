c4cda0d2776de5b7d994c9bf0e6766dfc284ef12
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-05-13 08:41:14 +0200

    entity mock builders

---
+++ src/Blauhaus.Domain.TestHelpers/MockBuilders/Common/Entities/BaseEntityMockBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Blauhaus.Domain.Common.Entities;
+using Blauhaus.TestHelpers.MockBuilders;
+
+namespace Blauhaus.Domain.TestHelpers.MockBuilders.Common.Entities
+{
+
+    public class EntityMockBuilder : BaseEntityMockBuilder<EntityMockBuilder, IEntity>
+    {
+    }
+
+    public class BaseEntityMockBuilder<TBuilder, TMock> : BaseMockBuilder<TBuilder, TMock>
+        where TMock : class, IEntity
+        where TBuilder : BaseEntityMockBuilder<TBuilder, TMock>
+    {
+        public BaseEntityMockBuilder()
+        {
+            With(x => x.Id, Guid.NewGuid());
+            With(x => x.EntityState, EntityState.Active);
+        }
+    }
+}
---
+++ src/Blauhaus.Domain.TestHelpers/MockBuilders/Common/Entities/BaseServerEntityMockBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Blauhaus.Domain.Common.Entities;
+using Blauhaus.TestHelpers.MockBuilders;
+
+namespace Blauhaus.Domain.TestHelpers.MockBuilders.Common.Entities
+{
+    public class ServerEntityMockBuilder : BaseServerEntityMockBuilder<ServerEntityMockBuilder, IServerEntity>
+    {
+    }
+
+
+    public abstract class BaseServerEntityMockBuilder<TBuilder, TMock> : BaseEntityMockBuilder<TBuilder, TMock> 
+        where TBuilder : BaseServerEntityMockBuilder<TBuilder, TMock>
+        where TMock : class, IServerEntity
+    {
+        protected BaseServerEntityMockBuilder()
+        {
+            With(x => x.CreatedAt, DateTime.UtcNow.AddDays(-2));
+            With(x => x.ModifiedAt, DateTime.UtcNow.AddDays(-1));
+        }
+    }
+}
