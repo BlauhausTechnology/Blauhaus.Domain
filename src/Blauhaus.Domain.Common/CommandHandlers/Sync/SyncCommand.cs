b032bba9c47a6b3394ae4cd1331380a67801d7c3
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-06-28 10:22:56 +0200

    sync by Id

--- src/Blauhaus.Domain.Common/CommandHandlers/Sync/SyncCommand.cs
+++ src/Blauhaus.Domain.Common/CommandHandlers/Sync/SyncCommand.cs
@@ -20,7 +20,11 @@ namespace Blauhaus.Domain.Common.CommandHandlers.Sync
         /// Specifies the number of entities to be downloaded from the server and published to the client in each batch
         /// </summary>
         public int BatchSize { get; set; } = 100;
-        
+
+        /// <summary>
+        /// If provided then only the entity matching this Id will be returned, and only NewerThan will be considered
+        /// </summary>
+        public Guid? Id { get; set; } = null;
 
     }
 }
