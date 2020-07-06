0660e7b0b5548aff41230741bef3b45966fe10ba
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-06 16:33:48 +0200

    no message

---
+++ src/Blauhaus.Domain.TestHelpers/MockBuilders/Client/SyncCollections/SyncCollectionMockBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Blauhaus.Domain.Client.Sync.Collection;
+using Blauhaus.Domain.Common.CommandHandlers.Sync;
+using Blauhaus.Domain.Common.Entities;
+
+namespace Blauhaus.Domain.TestHelpers.MockBuilders.Client.SyncCollections
+{
+    public class SyncCollectionMockBuilder<TModel, TListItem> : BaseSyncCollectionMockBuilder<SyncCollectionMockBuilder<TModel, TListItem>, TModel, TListItem, SyncCommand>
+        where TListItem : IListItem<TModel>
+        where TModel : IClientEntity
+    {
+    }
+
+    public class SyncCollectionMockBuilder<TModel, TListItem, TSyncCommand> : BaseSyncCollectionMockBuilder<SyncCollectionMockBuilder<TModel, TListItem, TSyncCommand>, TModel, TListItem, TSyncCommand>
+        where TSyncCommand : SyncCommand, new()
+        where TListItem : IListItem<TModel>
+        where TModel : IClientEntity
+    {
+    }
+
+}
