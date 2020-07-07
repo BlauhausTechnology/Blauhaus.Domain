4a8d4ec6f5fe5cc0149488eae529d01876fa818a
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-07 12:35:18 +0200

    no message

--- src/Blauhaus.Domain.Client/Sync/Collection/BaseListItem.cs
+++ src/Blauhaus.Domain.Client/Sync/Collection/BaseListItem.cs
@@ -4,24 +4,23 @@ using Blauhaus.Domain.Common.Entities;
 
 namespace Blauhaus.Domain.Client.Sync.Collection
 {
-    public abstract class ListItem<TModel> : BaseBindableObject, IListItem<TModel> where TModel : IClientEntity
+    public abstract class BaseListItem<TModel> : BaseBindableObject, IListItem<TModel> where TModel : IClientEntity
     { 
 
         public Guid Id { get; set; }
         public long ModifiedAtTicks { get; set; }
         public EntityState EntityState { get; private set; } = EntityState.Active;
-        public bool IsVisible { get; set; } //todo
 
-        public virtual void UpdateFromModel(TModel model)
+        public virtual bool UpdateFromModel(TModel model)
         {
             Id = model.Id;
             ModifiedAtTicks = model.ModifiedAtTicks;
             EntityState = model.EntityState;
 
-            Update(model);
+            return Update(model);
         }
 
-        protected abstract void Update(TModel model);
+        protected abstract bool Update(TModel model);
 
     }
 }
