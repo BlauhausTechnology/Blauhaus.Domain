c637cdf6fd2cc982cc5c15c575bb5d791ac7eac0
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-09 17:02:32 +0200

    no message

---
+++ src/Blauhaus.Domain.Common/Errors/DomainErrors.cs
@@ -0,0 +1,14 @@
+﻿using Blauhaus.Errors;
+
+namespace Blauhaus.Domain.Common.Errors
+{
+    public class DomainErrors
+    {
+        public static Error NotFound(string name = "") => string.IsNullOrEmpty(name) 
+            ? Error.Create("Not found") 
+            : Error.Create($"{name} Not found");
+
+        public static Error InvalidDateFormat = Error.Create("Dates and times must be in UTC format");
+         
+    }
+}
