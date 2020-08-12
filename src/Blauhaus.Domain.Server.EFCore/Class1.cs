966d8ab71da6bc85578b435d3eb5501f30a04ce5
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-12 16:08:15 +0200

    no message

---
+++ src/Blauhaus.Domain.Server.EFCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Blauhaus.Domain.Server.EfCore
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Blauhaus.Domain.TestHelpers.EFCore/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Blauhaus.Domain.TestHelpers.EFCore
+{
+    public class Class1
+    {
+    }
+}
