ee7284c635f9450a03e87542b80dc8ac4fae5645
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-17 17:45:17 +0200

    no message

--- src/Blauhaus.Domain.Tests/ClientTests/SqliteTests/_Base/BaseSqliteTest.cs
+++ src/Blauhaus.Domain.Tests/ClientTests/SqliteTests/_Base/BaseSqliteTest.cs
@@ -25,7 +25,7 @@ namespace Blauhaus.Domain.Tests.ClientTests.SqliteTests._Base
         {
             Cleanup();            
             SqliteDatabaseService = new SqliteInMemoryDatabase(new SqliteConfig(MockDeviceInfoService.Object));
-            Task.Run(async () => await SqliteDatabaseService.DropTablesAsync()).Wait();
+            Task.Run(async () => await SqliteDatabaseService.DeleteDataAsync()).Wait();
             Connection = SqliteDatabaseService.GetDatabaseConnectionAsync().Result;
             AddService(SqliteDatabaseService);
             AddService(x => MockClientEntityConverter.Object);
