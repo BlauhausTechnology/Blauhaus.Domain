e1040a247c8184f9d5a0a9cc02002c75d3738424
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-20 14:30:21 +0200

    no message

--- src/Blauhaus.Domain.Abstractions/Errors/DomainErrors.cs
+++ src/Blauhaus.Domain.Abstractions/Errors/DomainErrors.cs
@@ -8,11 +8,16 @@ namespace Blauhaus.Domain.Abstractions.Errors
 {
     public class DomainErrors
     {
+
+        #region NotFound
+
         public static Error NotFound() => Error.Create("Entity not found");
         public static Error NotFound(string name) => Error.Create($"{name} not found");
         public static Error NotFound<T>() => Error.Create($"{typeof(T).Name} not found");
 
-        
+        #endregion
+
+        #region Duplicate
         public static Error Duplicate() 
             => Error.Create("This entity already exists");
 
@@ -28,11 +33,25 @@ namespace Blauhaus.Domain.Abstractions.Errors
         public static Error Duplicate<T>(Expression<Func<T, object>> expression, object value) 
             => Error.Create($"{typeof(T).Name} already exists with {expression.ToPropertyName()} == {value}");
 
-        public static Error InvalidState() => Error.Create("Entity state is invalid for this operation");
-        public static Error InvalidState(EntityState state) => Error.Create($"{state} is an invalid state for this operation");
+        #endregion
 
+        #region InvalidEntityState
+        
+        public static Error InvalidEntityState() => Error.Create("Entity state is invalid for this operation");
+        public static Error InvalidEntityState(EntityState state) => Error.Create($"{state} is an invalid state for this operation");
 
+        #endregion
+
+        #region InvalidDateFormat
         public static Error InvalidDateFormat = Error.Create("Dates and times must be in UTC format");
-         
+        #endregion
+
+        #region InvalidCommand
+
+        public static Error InvalidCommand(string propertyName) => Error.Create($"The value provided for {propertyName} on the command was invalid");
+        public static Error InvalidCommand<TCommand>() => Error.Create($"The {typeof(TCommand).Name} was invalid");
+        public static Error InvalidCommand<TCommand>(string propertyName) => Error.Create($"The value provided for {propertyName} on {typeof(TCommand).Name} was invalid");
+
+        #endregion
     }
 }
