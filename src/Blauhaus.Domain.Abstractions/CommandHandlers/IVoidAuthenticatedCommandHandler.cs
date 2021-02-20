da38f191941d44832e1426fd2fec5b981b1f31ff
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-02-20 13:47:44 +0200

    no message

--- src/Blauhaus.Domain.Abstractions/CommandHandlers/IVoidAuthenticatedCommandHandler.cs
+++ src/Blauhaus.Domain.Abstractions/CommandHandlers/IVoidAuthenticatedCommandHandler.cs
@@ -1,8 +1,14 @@
 ﻿using System.Threading.Tasks;
+using Blauhaus.Auth.Abstractions.User;
 using Blauhaus.Responses;
 
 namespace Blauhaus.Domain.Abstractions.CommandHandlers
 {
+    public interface IVoidAuthenticatedCommandHandler<in TCommand> : IVoidAuthenticatedCommandHandler<TCommand, IAuthenticatedUser>
+        where TCommand : notnull
+    {
+    } 
+    
     public interface IVoidAuthenticatedCommandHandler<in TCommand, in TUser> 
         where TCommand : notnull
         where TUser : notnull
--- src/Blauhaus.Domain.Abstractions/Commands/DeleteCommand.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Blauhaus.Domain.Abstractions.Commands
-{
-    public class DeleteCommand
-    {
-        
-    }
-}
