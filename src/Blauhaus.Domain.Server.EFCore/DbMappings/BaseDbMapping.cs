60d7852fa74833b2a02a073e72a191d57b92f9de
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-04-22 08:48:38 +0200

    no message

--- src/Blauhaus.Domain.Server.EFCore/DbMappings/BaseDbMapping.cs
+++ src/Blauhaus.Domain.Server.EFCore/DbMappings/BaseDbMapping.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Blauhaus.Domain.Server.EFCore.Entities;
+using Blauhaus.Domain.Server.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
