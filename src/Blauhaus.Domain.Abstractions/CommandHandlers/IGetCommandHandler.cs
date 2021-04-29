fb1d6ee344a9daaa7310ec3771e311cc0768d0d5
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-04-29 12:14:56 +0200

    no message

---
+++ src/Blauhaus.Domain.Abstractions/CommandHandlers/IGetCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Blauhaus.Domain.Abstractions.Commands;
+using Blauhaus.Domain.Abstractions.Dtos;
+
+namespace Blauhaus.Domain.Abstractions.CommandHandlers
+{
+    public interface IGetCommandHandler<TDto, in TUser>
+    {
+        Task<GetResultDto<TDto>> GetAsync(GetCommand command, TUser user);
+    }
+}
