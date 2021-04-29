c86e8b9a279f082d3dc04ba51464b5313d554a3c
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-04-29 12:12:37 +0200

    no message

--- src/Blauhaus.Domain.Abstractions/Commands/GetCommand.cs
+++ src/Blauhaus.Domain.Abstractions/Commands/GetCommand.cs
@@ -2,6 +2,8 @@
 {
     public class GetCommand
     {
-        
+        public long? ModifiedAfter { get; set; }
+        public long? ModifiedBefore { get; set; }
+        public int BatchSize { get; set; } = 100;
     }
 }
---
+++ src/Blauhaus.Domain.Abstractions/Dtos/GetResultDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Blauhaus.Domain.Abstractions.Dtos
+{
+    public class GetResultDto<TDto>
+    {
+        public List<TDto> CurrentBatch { get; set; } = new List<TDto>();
+        public int TotalCount { get; set; }
+        public int TotalPageCount { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
