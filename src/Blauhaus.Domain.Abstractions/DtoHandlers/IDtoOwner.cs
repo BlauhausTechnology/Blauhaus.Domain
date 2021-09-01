1ac845e732d8468e41b8a62bcf0a43059e40b027
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-09-01 17:50:34 +0200

    no message

--- src/Blauhaus.Domain.Abstractions/DtoHandlers/IDtoOwner.cs
+++ src/Blauhaus.Domain.Abstractions/DtoHandlers/IDtoOwner.cs
@@ -1,15 +1,8 @@
-﻿using Blauhaus.Domain.Abstractions.Entities;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace Blauhaus.Domain.Abstractions.DtoHandlers
-{
-    public interface IDtoOwner<TDto, TId> where TDto : IClientEntity<TId> 
-    {
-        Task<TDto> GetDtoAsync();
-    }
-
-    public interface IDtoOwner<TDto> where TDto : IClientEntity
+{ 
+    public interface IDtoOwner<TDto> 
     {
         Task<TDto> GetDtoAsync();
     }
