47949f5f83d393ed28c55f2b8aed19f51d61b5d1
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-10-26 07:51:36 +0200

    no message

---
+++ src/Blauhaus.Domain.Abstractions/CommandHandlers/IEntityLifecycleHandler.cs
@@ -0,0 +1,12 @@
+﻿using Blauhaus.Auth.Abstractions.User;
+using Blauhaus.Domain.Abstractions.Commands;
+
+namespace Blauhaus.Domain.Abstractions.CommandHandlers
+{
+    public interface IEntityLifecycleHandler :
+        IVoidAuthenticatedCommandHandler<ActivateCommand, IAuthenticatedUser>,
+        IVoidAuthenticatedCommandHandler<ArchiveCommand, IAuthenticatedUser>,
+        IVoidAuthenticatedCommandHandler<DeleteCommand, IAuthenticatedUser>
+    {
+    }
+}
