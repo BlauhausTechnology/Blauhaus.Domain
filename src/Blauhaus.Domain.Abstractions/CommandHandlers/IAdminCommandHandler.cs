f0286cd653592466abe3621e7ba87e1d694d7ced
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-01 13:06:20 +0200

    no message

---
+++ src/Blauhaus.Domain.Abstractions/CommandHandlers/IAdminCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using Blauhaus.Auth.Abstractions.User;
+using Blauhaus.Domain.Abstractions.Commands;
+
+namespace Blauhaus.Domain.Abstractions.CommandHandlers
+{
+    public interface IAdminCommandHandler<in TCommand> : IVoidAuthenticatedCommandHandler<TCommand, IAuthenticatedUser> 
+        where TCommand : IAdminCommand
+    {
+    }
+     
+    public interface IAdminCommandHandler<TResponse, in TCommand> : IAuthenticatedCommandHandler<TResponse, TCommand, IAuthenticatedUser> 
+        where TCommand : IAdminCommand
+    {
+    }
+
+}
