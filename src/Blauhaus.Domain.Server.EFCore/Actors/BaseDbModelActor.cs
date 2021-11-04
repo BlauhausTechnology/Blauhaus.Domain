c8b58bba5fd3f3ed59ff432ec5f3760e111aa262
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-04 15:36:42 +0200

    no message

--- src/Blauhaus.Domain.Server.EFCore/Actors/BaseDbModelActor.cs
+++ src/Blauhaus.Domain.Server.EFCore/Actors/BaseDbModelActor.cs
@@ -2,10 +2,15 @@
 using System.Threading.Tasks;
 using Blauhaus.Analytics.Abstractions.Extensions;
 using Blauhaus.Analytics.Abstractions.Service;
+using Blauhaus.Auth.Abstractions.Errors;
+using Blauhaus.Auth.Abstractions.Extensions;
+using Blauhaus.Auth.Abstractions.User;
 using Blauhaus.Common.Abstractions;
+using Blauhaus.Domain.Abstractions.Commands;
 using Blauhaus.Domain.Server.Actors;
 using Blauhaus.Domain.Server.Entities;
 using Blauhaus.Errors;
+using Blauhaus.Errors.Extensions;
 using Blauhaus.Responses;
 using Blauhaus.Time.Abstractions;
 using Microsoft.EntityFrameworkCore;
@@ -32,15 +37,23 @@ namespace Blauhaus.Domain.Server.EFCore.Actors
             TimeService = timeService;
         }
 
-        protected async Task<Response> TryExecuteCommandAsync<TCommand>(TCommand command, Func<TDbContext, Task<Response>> func)
+        
+        protected async Task<Response> TryExecuteCommandAsync<TCommand>(TCommand command, IAuthenticatedUser user, Func<TDbContext, DateTime, Task<Response>> func)
         {
             using (var _ = AnalyticsService.StartTrace(this, $"{typeof(TCommand).Name} executed by {this.GetType().Name}", LogSeverity.Verbose, command.ToObjectDictionary()))
             {
                 try
                 {
+                    if (command is IAdminCommand)
+                    {
+                        if (!user.IsAdminUser())
+                        {
+                            return AnalyticsService.TraceErrorResponse(this, AuthError.NotAuthorized);
+                        }
+                    }
                     using (var db = GetDbContext)
                     {
-                        var response = await func.Invoke(db);
+                        var response = await func.Invoke(db, TimeService.CurrentUtcTime);
                         if (response.IsSuccess && db.ChangeTracker.HasChanges())
                         {
                             await db.SaveChangesAsync();
@@ -50,11 +63,50 @@ namespace Blauhaus.Domain.Server.EFCore.Actors
                 }
                 catch (Exception e)
                 {
+                    if (e.IsErrorException())
+                    {
+                        return AnalyticsService.TraceErrorResponse(this, e.ToError());
+                    }
                     AnalyticsService.LogException(this, e, command.ToObjectDictionary());
                     return Response.Failure(Error.Unexpected($"{typeof(TCommand).Name} failed to complete"));
                 }
             }
-        }
+        } 
+
+        protected async Task<Response<T>> TryExecuteCommandAsync<T, TCommand>(TCommand command, IAuthenticatedUser user, Func<TDbContext, DateTime, Task<Response<T>>> func)
+        {
+            using (var _ = AnalyticsService.StartTrace(this, $"{typeof(TCommand).Name} executed by {this.GetType().Name}", LogSeverity.Verbose, command.ToObjectDictionary()))
+            {
+                try
+                {
+                    if (command is IAdminCommand)
+                    {
+                        if (!user.IsAdminUser())
+                        {
+                            return AnalyticsService.TraceErrorResponse<T>(this, AuthError.NotAuthorized);
+                        }
+                    }
+                    using (var db = GetDbContext)
+                    {
+                        var response = await func.Invoke(db, TimeService.CurrentUtcTime);
+                        if (response.IsSuccess && db.ChangeTracker.HasChanges())
+                        {
+                            await db.SaveChangesAsync();
+                        }
+                        return response;
+                    }
+                }
+                catch (Exception e)
+                {
+                    if (e.IsErrorException())
+                    {
+                        return AnalyticsService.TraceErrorResponse<T>(this, e.ToError());
+                    }
+                    AnalyticsService.LogException(this, e, command.ToObjectDictionary());
+                    return Response.Failure<T>(Error.Unexpected($"{typeof(TCommand).Name} failed to complete"));
+                }
+            }
+        } 
         
     }
 }
