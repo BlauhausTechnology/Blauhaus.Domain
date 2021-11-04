f6789b17e0db91141695b8f99acd69a66e033c26
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-04 09:59:29 +0200

    no message

--- src/Blauhaus.Domain.Client/DtoCaches/InMemoryDtoCache.cs
+++ src/Blauhaus.Domain.Client/DtoCaches/InMemoryDtoCache.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
-using Blauhaus.ClientActors.Actors;
 using Blauhaus.Common.Abstractions;
+using Blauhaus.Common.Utils.Disposables;
 using Blauhaus.Domain.Abstractions.DtoCaches;
 using Blauhaus.Domain.Abstractions.DtoHandlers;
 using Blauhaus.Domain.Abstractions.Errors;
@@ -12,82 +12,71 @@ using Blauhaus.Errors;
 
 namespace Blauhaus.Domain.Client.DtoCaches
 {
-    public class InMemoryDtoCache<TDto, TId> : BaseActor, IDtoCache<TDto, TId>
+    public class InMemoryDtoCache<TDto, TId> : BasePublisher, IDtoCache<TDto, TId>
         where TDto : class, IHasId<TId> where TId : IEquatable<TId>
     {
 
         protected Dictionary<TId, TDto> CachedDtos = new Dictionary<TId, TDto>();
 
-        public Task HandleAsync(TDto dto)
+        public async Task HandleAsync(TDto dto)
         {
-            return InvokeAsync(async () =>
-            {
-                CachedDtos[dto.Id] = dto;
-                await UpdateSubscribersAsync(dto);
-            });
+            CachedDtos[dto.Id] = dto;
+            await UpdateSubscribersAsync(dto);
         }
 
         public Task<IDisposable> SubscribeAsync(Func<TDto, Task> handler, Func<TDto, bool>? filter = null)
         {
-            return InvokeAsync(() => AddSubscriber(handler, filter));
+            return Task.FromResult(AddSubscriber(handler, filter));
         }
 
         public Task<TDto> GetOneAsync(TId id)
         {
-            return InvokeAsync(() =>
+            if (CachedDtos.TryGetValue(id, out var dto))
             {
-                if (CachedDtos.TryGetValue(id, out var dto))
-                {
-                    return dto;
-                }
-                throw new ErrorException(DomainErrors.NotFound<TDto>());
-            });
+                return Task.FromResult(dto);
+            }
+            throw new ErrorException(DomainErrors.NotFound<TDto>());
         }
 
         public Task<TDto?> TryGetOneAsync(TId id)
         {
-            return InvokeAsync(() =>
-                CachedDtos.TryGetValue(id, out var dto)
+            return Task.FromResult(CachedDtos
+                .TryGetValue(id, out var dto)
                     ? dto
                     : null);
         }
 
         public Task<IReadOnlyList<TDto>> GetAllAsync()
         {
-            return InvokeAsync<IReadOnlyList<TDto>>(() =>
-                CachedDtos.Values.ToList());
+            return Task.FromResult<IReadOnlyList<TDto>>(CachedDtos.Values.ToList());
         }
 
         public Task<IReadOnlyList<TDto>> GetWhereAsync(Func<TDto, bool> filter)
         {
-            return InvokeAsync<IReadOnlyList<TDto>>(() => 
-                CachedDtos.Values.Where(filter).ToList());
+            return Task.FromResult<IReadOnlyList<TDto>>(CachedDtos.Values.Where(filter).ToList());
         }
 
         public Task<IReadOnlyList<TId>> GetIdsWhereAsync(Func<TDto, bool> filter)
         {
-            return InvokeAsync<IReadOnlyList<TId>>(() => 
+            return Task.FromResult<IReadOnlyList<TId>>(
                 CachedDtos.Values.Where(filter).Select(y => y.Id).ToList());
         }
 
         public Task DeleteOneAsync(TId id)
         {
-            return InvokeAsync(() =>
+            if (CachedDtos.TryGetValue(id, out _))
             {
-                if (CachedDtos.TryGetValue(id, out _))
-                {
-                    CachedDtos.Remove(id);
-                }
-            });
+                CachedDtos.Remove(id);
+            }
+
+            return Task.CompletedTask;
 
         }
 
         public Task DeleteAllAsync()
         {
-            return InvokeAsync(() =>
-            {
-                CachedDtos.Clear();
-            });
+            CachedDtos.Clear();
+            return Task.CompletedTask;
         }
     }
 }
