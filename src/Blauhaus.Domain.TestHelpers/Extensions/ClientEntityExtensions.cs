f5a22aea9bec34e9c61f8c562d3ab92cc9c555e7
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-11 11:51:48 +0200

    no message

---
+++ src/Blauhaus.Domain.TestHelpers/Extensions/ClientEntityExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Blauhaus.Domain.Abstractions.Entities;
+using NUnit.Framework;
+
+namespace Blauhaus.Domain.TestHelpers.Extensions
+{
+    public static class ClientEntityExtensions 
+    {
+        public static void VerifyAgainstServerEntity(this IClientEntity<Guid> clientEntity, IServerEntity serverEntity)
+        {
+            Assert.That(clientEntity.Id, Is.EqualTo(serverEntity.Id));
+            Assert.That(clientEntity.EntityState, Is.EqualTo(serverEntity.EntityState));
+            Assert.That(clientEntity.ModifiedAtTicks, Is.EqualTo(serverEntity.ModifiedAt.Ticks));
+        }
+    }
+}
