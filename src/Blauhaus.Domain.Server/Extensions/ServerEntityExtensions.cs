d788b7902c9f483ab85246ffd2bcd73ff2b9b446
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-11-19 14:00:49 +0200

    no message

---
+++ src/Blauhaus.Domain.Server/Extensions/ServerEntityExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Blauhaus.Domain.Abstractions.Entities;
+using Blauhaus.Domain.Abstractions.Errors;
+using Blauhaus.Domain.Server.Entities;
+using Blauhaus.Responses;
+
+namespace Blauhaus.Domain.Server.Extensions
+{
+    public static class ServerEntityExtensions
+    {
+        public static Response TryDelete(this BaseServerEntity entity, DateTime now)
+        {
+            switch (entity.EntityState)
+            {
+                case EntityState.Deleted:
+                    return Response.Success();
+                case EntityState.Active:
+                    return Response.Failure(DomainErrors.InvalidEntityState(entity.EntityState));
+                case EntityState.Draft:
+                case EntityState.Archived:
+                    entity.Delete(now);
+                    return Response.Success();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public static Response TryArchive(this BaseServerEntity entity, DateTime now)
+        {
+            switch (entity.EntityState)
+            {
+                case EntityState.Archived:
+                    return Response.Success();
+                case EntityState.Draft:
+                case EntityState.Deleted:
+                    return Response.Failure(DomainErrors.InvalidEntityState(entity.EntityState));
+                case EntityState.Active:
+                    entity.Archive(now);
+                    return Response.Success();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        
+        public static Response TryActivate(this BaseServerEntity entity, DateTime now)
+        {
+            switch (entity.EntityState)
+            {
+                case EntityState.Active:
+                    return Response.Success();
+                case EntityState.Deleted:
+                    return Response.Failure(DomainErrors.InvalidEntityState(entity.EntityState));
+                case EntityState.Draft:
+                case EntityState.Archived:
+                    entity.Activate(now);
+                    return Response.Success();
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        
+        public static bool CanRemove(this BaseServerEntity entity)
+        {
+            return entity.EntityState == EntityState.Deleted;
+        }
+    }
+}
