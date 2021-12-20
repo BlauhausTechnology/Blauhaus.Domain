5ab3b0ae0e6d8a628f95d7f089e7ae0795a288e6
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2021-12-20 16:48:37 +0200

    no message

--- src/Blauhaus.Domain.TestHelpers/BaseTests/BaseSqliteTest.cs
+++ src/Blauhaus.Domain.TestHelpers/BaseTests/BaseSqliteTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Blauhaus.Analytics.Abstractions.Service;
 using Blauhaus.Analytics.TestHelpers.MockBuilders;
@@ -6,7 +7,9 @@ using Blauhaus.ClientDatabase.Sqlite.Config;
 using Blauhaus.ClientDatabase.Sqlite.Service;
 using Blauhaus.DeviceServices.Abstractions.DeviceInfo;
 using Blauhaus.DeviceServices.TestHelpers.MockBuilders;
+using Blauhaus.Domain.Server.Entities;
 using Blauhaus.TestHelpers.BaseTests;
+using Blauhaus.TestHelpers.Builders.Base;
 using NUnit.Framework;
 using SQLite;
 
@@ -16,6 +19,8 @@ namespace Blauhaus.Domain.TestHelpers.BaseTests
         where TSut : class
         where TSqliteConfig : BaseSqliteConfig
     {
+        
+        private readonly List<Action<SQLiteAsyncConnection>> _entityFactories = new();   
         protected ISqliteDatabaseService SqliteDatabaseService = null!;
         protected SQLiteAsyncConnection Connection = null!;
 
@@ -23,6 +28,8 @@ namespace Blauhaus.Domain.TestHelpers.BaseTests
         public virtual void Setup()
         {
             base.Cleanup();
+            
+            _entityFactories.Clear();
 
             var config = (ISqliteConfig)Activator.CreateInstance(typeof(TSqliteConfig), MockDeviceInfoService.Object);
             SqliteDatabaseService = new SqliteInMemoryDatabase(config);
@@ -36,5 +43,35 @@ namespace Blauhaus.Domain.TestHelpers.BaseTests
 
         protected DeviceInfoServiceMockBuilder MockDeviceInfoService => AddMock<DeviceInfoServiceMockBuilder, IDeviceInfoService>().Invoke();
         protected AnalyticsServiceMockBuilder MockAnalyticsService => AddMock<AnalyticsServiceMockBuilder, IAnalyticsService>().Invoke();
+
+        protected override void BeforeConstructSut(IServiceProvider serviceProvider)
+        {
+            base.BeforeConstructSut(serviceProvider);
+
+            foreach (var setupFunc in _entityFactories)
+            {
+                setupFunc.Invoke(Connection);
+            }
+        }
+
+        protected void AddEntityBuilders<T>(params IBuilder<T>[] builders) where T : BaseServerEntity 
+        {
+            foreach (var builder in builders)
+            {
+                _entityFactories.Add(connection=> Task.Run(async ()=> await connection.InsertAsync(builder.Object)).Wait());
+            } 
+        }
+        protected void AddEntityBuilder<T>(IBuilder<T> builder) where T : BaseServerEntity 
+        {
+            _entityFactories.Add(connection=> Task.Run(async ()=> await connection.InsertAsync(builder.Object)).Wait());
+        }
+
+        protected void AddEntityBuilders<T>(List<IBuilder<T>> builders) where T : BaseServerEntity 
+        {
+            foreach (var builder in builders)
+            {
+                _entityFactories.Add(connection=> Task.Run(async ()=> await connection.InsertAsync(builder.Object)).Wait());
+            } 
+        }
     }
 }
