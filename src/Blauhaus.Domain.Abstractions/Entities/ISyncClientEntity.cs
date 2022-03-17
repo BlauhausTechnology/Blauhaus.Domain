00f45399865200c44df753d2a1557c200b71ee6c
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2022-03-17 14:49:54 +0200

    no message

---
+++ src/Blauhaus.Domain.Abstractions/Entities/ISyncClientEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blauhaus.Domain.Abstractions.Entities
+{
+    public interface ISyncClientEntity<out TId> : IClientEntity<TId>
+    {
+        SyncState SyncState { get; set; }
+    }
+}
---
+++ src/Blauhaus.Domain.Abstractions/Entities/SyncState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blauhaus.Domain.Abstractions.Entities
+{
+    public enum SyncState
+    {
+        OutOfSync,
+        InSync,
+    }
+}
